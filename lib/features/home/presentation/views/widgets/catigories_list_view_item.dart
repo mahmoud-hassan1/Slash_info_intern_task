0271251378cb580abd094299e36d944de27d0ac0
mahmoud <mahmoudhassan10191019@gmail.com>
2024-06-22 03:55:56 +0300

    Catigory List View Item

---
+++ lib/features/home/presentation/views/widgets/catigories_list_view_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:slash_info_task/core/utiles/assets.dart';
+import 'package:slash_info_task/core/utiles/fonts.dart';
+
+class CategoryListViewItem extends StatelessWidget {
+  const CategoryListViewItem({
+    super.key,
+    required this.index
+  });
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SvgPicture.asset(Assets.kCategories[index][1],),
+        Text(
+         Assets.kCategories[index][0],
+         style: FontStyles.kSmallTextStyle,
+         )
+      ],
+    );
+  }
+}
