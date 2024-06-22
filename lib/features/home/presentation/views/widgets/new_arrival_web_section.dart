db0940a35516d857b99babdb03cb95d15881fbb0
mahmoud <mahmoudhassan10191019@gmail.com>
2024-06-22 22:52:59 +0300

    add new arrival and recommendes section

---
+++ lib/features/home/presentation/views/widgets/new_arrival_web_section.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:slash_info_task/features/home/presentation/manger/product_cubit.dart';
+import 'package:slash_info_task/features/home/presentation/views/widgets/header_titles.dart';
+import 'package:slash_info_task/features/home/presentation/views/widgets/product_grid_view_item.dart';
+import 'package:slash_info_task/features/home/presentation/views/widgets/product_list_view.dart';
+import 'package:slash_info_task/features/home/presentation/views/widgets/product_web_grid_view.dart';
+
+class NewArrivalWebSection extends StatelessWidget {
+  const NewArrivalWebSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+      return  Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+         const Header(
+          title: "New Arrival",
+        ),
+         const SizedBox(
+          height: 16,
+        ),
+        ProductWebGridView(products: BlocProvider.of<ProductCubit>(context).newArrival!,)
+      ],
+    );
+  }
+}
---
+++ lib/features/home/presentation/views/widgets/products_web_section.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:slash_info_task/core/utiles/fonts.dart';
+import 'package:slash_info_task/features/home/presentation/manger/product_cubit.dart';
+import 'package:slash_info_task/features/home/presentation/views/widgets/best_seller_section.dart';
+import 'package:slash_info_task/features/home/presentation/views/widgets/best_seller_web_section.dart';
+import 'package:slash_info_task/features/home/presentation/views/widgets/new_arival_section.dart';
+import 'package:slash_info_task/features/home/presentation/views/widgets/new_arrival_web_section.dart';
+import 'package:slash_info_task/features/home/presentation/views/widgets/recommended_section.dart';
+import 'package:slash_info_task/features/home/presentation/views/widgets/recommended_web_section.dart';
+
+class ProductsWebSection extends StatelessWidget {
+  const ProductsWebSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProductCubit, ProductState>(
+      builder: (context, state) {
+        if(state is ProductSuccess){
+        return const Column(
+          children: [
+            BestSellerWebSection(),
+            SizedBox(
+              height: 16,
+            ),
+            NewArrivalWebSection(),
+            SizedBox(
+              height: 16,
+            ),
+            RecommendedWebSection(),
+          ],
+        );
+        }
+        else if(state is ProductLoading){
+          return  Center(child: Text("Loading",style: FontStyles.kLargeTextStyle(context),),);
+        }
+        else{
+          return  Center(child: Text("Error",style: FontStyles.kLargeTextStyle(context),),);
+        }
+      },
+    );
+  }
+}
