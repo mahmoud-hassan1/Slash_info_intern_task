a167dfd3af34c3d5efe0b3a8fe88251879fa9112
mahmoud <mahmoudhassan10191019@gmail.com>
2024-06-22 22:53:15 +0300

    fixing product mobile section

--- lib/features/home/presentation/views/widgets/products_section.dart
+++ lib/features/home/presentation/views/widgets/products_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:slash_info_task/core/utiles/fonts.dart';
 import 'package:slash_info_task/features/home/presentation/manger/product_cubit.dart';
 import 'package:slash_info_task/features/home/presentation/views/widgets/best_seller_section.dart';
 import 'package:slash_info_task/features/home/presentation/views/widgets/new_arival_section.dart';
@@ -12,26 +13,34 @@ class ProductsSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<ProductCubit, ProductState>(
       builder: (context, state) {
-        if(state is ProductSuccess){
-        return const Column(
-          children: [
-            BestSellerSection(),
-            SizedBox(
-              height: 16,
+        if (state is ProductSuccess) {
+          return const Column(
+            children: [
+              BestSellerSection(),
+              SizedBox(
+                height: 16,
+              ),
+              NewArrivalSection(),
+              SizedBox(
+                height: 16,
+              ),
+              RecommendedSection(),
+            ],
+          );
+        } else if (state is ProductLoading) {
+          return Center(
+            child: Text(
+              "Loading",
+              style: FontStyles.kLargeTextStyle(context),
             ),
-            NewArrivalSection(),
-            SizedBox(
-              height: 16,
+          );
+        } else {
+          return Center(
+            child: Text(
+              "Error",
+              style: FontStyles.kLargeTextStyle(context),
             ),
-            RecommendedSection(),
-          ],
-        );
-        }
-        else if(state is ProductLoading){
-          return const Center(child: Text("Loading"),);
-        }
-        else{
-          return const Center(child: Text("Error",style: TextStyle(fontSize: 50,color: Colors.black),),);
+          );
         }
       },
     );
