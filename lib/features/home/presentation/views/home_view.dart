edb8eb828d4241eeeda8c1cf9a192210c9e3d22c
mahmoud <mahmoudhassan10191019@gmail.com>
2024-06-22 16:17:31 +0300

    add bottom navigation bar

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:slash_info_task/core/utiles/fonts.dart';
 import 'package:slash_info_task/features/home/presentation/views/widgets/home_vie_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -6,8 +8,37 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body:HomeViewBody() ,
+    return Scaffold(
+      body: const HomeViewBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_rounded)  ,
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border_outlined),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_2_outlined),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: 0,
+        onTap: (index) {},
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+        showSelectedLabels: true,
+
+        selectedLabelStyle:  FontStyles.kSmallTextStyle.copyWith(fontWeight: FontWeight.w700), 
+        unselectedLabelStyle: FontStyles.kSmallTextStyle, 
+      ),
     );
   }
 }
